86d5ce264f9dbc59577448ef931321efe33d79d0
Yann <yilboudo@gmail.com>
2017-07-03 23:08:23 -0400

    update permutation script

--- minp_permutations_v2.R
+++ minp_permutations_v2.R
@@ -1,76 +1,116 @@
-minp_permutations <- function(phenotype,input_filename, testStat = "linear", NFML, NLML, Nperm,output_filename, sex, age, ...)
+start.time <- proc.time()[3]
+options(echo = TRUE)
+options(warn=-1)
+library(batch)
+
+cat(" ################################# \n # \n # \n # \n # \n # Metabolomics Phenotype Association: \n # \n # Yann Ilboudo, Guillaume Lettre 2016 \n # \n #\n # \n ################################# \n")
+
+
+minp_permutations <- function(phenotype,input_filename, testStat = "linear", NFML, NLML, Nperm,output_filename, use.cov = F, cov_names, inv.norm=F)
 {
   set.seed(4)
   
   Metabo_Pheno_File<- read.table(input_filename, h=T, sep="\t")
   #Define Test Statistic and Phenotype of Interest. Pick which test statistic to run linear or logistic regression
   
-  
-  linearReg_p <- function(y,x,w,z) coef(summary(lm(y ~ x + w + z)))[2,4]
-  linearReg_stderr <- function(y,x,w,z) coef(summary(lm(y ~ x + w + z)))[2,2]
-  linearReg_effect <- function(y,x,w,z) coef( summary(lm(y ~ x + w + z)) ) [2,1]
-  linearReg_samplesize <- function(y,x, w, z) nrow(model.frame(lm(y ~ x + w + z)))
-
-  
-  logisticReg_p <- function(y,x, w, z) coef(summary(glm(y ~ x + w + z,family=binomial(link='logit'))))[2,4]
-  logisticReg_stderr <- function(y,x, w, z) coef(summary(glm(y ~ x + w + z,family=binomial(link='logit'))))[2,2]
-  logisticReg_effect <- function(y,x, w, z) coef(summary(glm(y ~ x + w + z,family=binomial(link='logit'))))[2,1]
-  logisticReg_samplesize <- function(y,x, w, z) nrow(model.frame(glm(y ~ x + w + z,family=binomial(link='logit'))))
-
-
 
   PofI= Metabo_Pheno_File[,which(colnames(Metabo_Pheno_File)==phenotype)]
-  if missing(sex){
-     Sex = NA} else {
-  Sex = Metabo_Pheno_File[,which(colnames(Metabo_Pheno_File)==sex)]
-  }
- 
-  if missing(age){
-     Age = NA} else {
-  
-  Age = Metabo_Pheno_File[,which(colnames(Metabo_Pheno_File)==age)]
-  }
 
-  #Name of first Metabolite in list and last
+  
+  #Name of first and last metabolite in file
   
   fmlist <- which(colnames(Metabo_Pheno_File)==NFML)
   lmlist <- which(colnames(Metabo_Pheno_File)==NLML)
-  
-  
-  if(testStat =="linear"){
 
-    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=PofI, w=Sex, z=Age)
-    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_stderr, y=PofI, w=Sex, z=Age)
-    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_effect, y=PofI, w=Sex, z=Age)
-    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_samplesize, y=PofI, w=Sex, z=Age)
+  covariates <- strsplit(cov_names, split=",")[[1]]
 
-  } else if(testStat=="logistic") {
-    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_p, y=PofI, w=Sex, z=Age)
-    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_stderr, y=PofI, w=Sex, z=Age)
-    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_effect, y=PofI, w=Sex, z=Age)
-    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_samplesize, y=PofI, w=Sex, z=Age)
-
-  } else {
-    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=PofI, w=Sex, z=Age)
-    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_stderr, y=PofI, w=Sex, z=Age)
-    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_effect, y=PofI, w=Sex, z=Age)
-    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_samplesize, y=PofI, w=Sex, z=Age)
-  }
+  cov=Metabo_Pheno_File[,covariates]
   
+  if (use.cov==T && testStat =="linear") {
+
+    
+    linearRegcov_p <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...) ,family="gaussian")))[2,4]
+    linearRegcov_stderr <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...) ,family="gaussian")))[2,2]
+    linearRegcov_effect <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...) ,family="gaussian")))[2,1]
+    linearRegcov_samplesize <- function(y,...) nrow(model.frame(glm(y ~ . , data.frame(y,...) ,family="gaussian")))
+      
+    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearRegcov_p, y=PofI, data=cov)
+    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearRegcov_stderr, y=PofI, data=cov)
+    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearRegcov_effect, y=PofI, data=cov)
+    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearRegcov_samplesize, y=PofI, data=cov)
+
+    } else if (use.cov == T && testStat=="logistic") {
+
+    logisticRegcov_p <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...) ,family="binomial")))[2,4]
+    logisticRegcov_stderr <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...),family="binomial")))[2,2]
+    logisticRegcov_effect <- function(y,...) coef(summary(glm(y ~ . , data.frame(y,...),family="binomial")))[2,1]
+    logisticRegcov_samplesize <- function(y,...) nrow(model.frame(glm(y ~ . , data.frame(y,...),family="binomial")))
+      
+    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticRegcov_p, y=PofI, data=cov)
+    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticRegcov_stderr, y=PofI, data=cov)
+    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticRegcov_effect, y=PofI, data=cov)
+    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticRegcov_samplesize, y=PofI, data=cov)
+      
+    } else if (use.cov == F && testStat=="linear") {
+
+    linearReg_p <- function(y,x) coef(summary(glm(y ~ x ,family="gaussian")))[2,4]
+    linearReg_stderr <- function(y,x) coef(summary(glm(y ~ x  ,family="gaussian")))[2,2]
+    linearReg_effect <- function(y,x) coef( summary(glm(y ~ x ,family="gaussian" )) ) [2,1]
+    linearReg_samplesize <- function(y,x) nrow(model.frame(glm(y ~ x ,family="gaussian")))
+
+    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=PofI)
+    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_stderr, y=PofI )
+    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_effect, y=PofI)
+    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_samplesize, y=PofI)
+
+    } else if (use.cov == F && testStat=="logistic") {
+
+    logisticReg_p <- function(y,x) coef(summary(glm(y ~ x ,family="binomial")))[2,4]
+    logisticReg_stderr <- function(y,x) coef(summary(glm(y ~ x ,family="binomial")))[2,2]
+    logisticReg_effect <- function(y,x) coef(summary(glm(y ~ x ,family="binomial")))[2,1]
+    logisticReg_samplesize <- function(y,x) nrow(model.frame(glm(y ~ x ,family="binomial")))
+    
+    observed_pval <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_p, y=PofI)
+    std_err <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_stderr, y=PofI)
+    effect_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_effect, y=PofI)
+    sample_size <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_samplesize, y=PofI)
+
+    } else {
+
+      print("Zero. Nothing to do.")
+    }
+  
+
+
+
+
   #Define Number of metabolites and number of simulations
   Nmetabo=length(fmlist:lmlist)
   permutations_pval <- matrix(NA,Nmetabo,Nperm)
   
   #Calculate test statistic for each permutations one for logistic one for linear
-  cat(" Calculating Permutation Matrix...\n")
-  if(testStat=="linear"){
-    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=sample(PofI, replace=F, size=length(PofI)), w=Sex, z=Age)}
-  }
-  else if(testStat=="logistic"){
-    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_p, y=sample(PofI, replace=F, size=length(PofI)),w=Sex, z=Age)}
+  cat(" \n \n ################################# \n # \n # Computing Permutation Matrix...\n # \n ################################# \n")
+  if (testStat=="linear" && use.cov==F) {
+
+    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=sample(PofI, replace=F, size=length(PofI)))}
+ 
+  } else if (testStat=="linear" && use.cov==T) {
+    
+    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearRegcov_p, y=sample(PofI, replace=F, size=length(PofI)), data=cov)}
+
+  } else if (testStat=="logistic" && use.cov==F) {
+
+    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticReg_p, y=sample(PofI, replace=F, size=length(PofI)))}
+  
+  } else if (testStat=="logistic" && use.cov==T) {
+
+    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], logisticRegcov_p, y=sample(PofI, replace=F, size=length(PofI)), data=cov)}
+  
   } else {
-    for (i in 1:Nperm) {permutations_pval[,i] <- sapply(Metabo_Pheno_File[,fmlist:lmlist], linearReg_p, y=sample(PofI, replace=F, size=length(PofI)),w=Sex, z=Age)}
+
+    print("Zero. Nothing to do.")
   }
+
   cat("\n")
   cat(" Finished!\n\n")
   #Store smallest pvalue for each rounds
@@ -92,5 +132,28 @@ minp_permutations <- function(phenotype,input_filename, testStat = "linear", NFM
   writeLines(paste("Metabolites","Sample Size","Effect Size","Standard Error","Observed_pvalue","Empirical_pvalue","Permutation_pvalue",sep="\t"), Outputfile)
   write.table(all_values, file = Outputfile, quote = FALSE, row.names = TRUE, col.names = FALSE,sep="\t")
   close(Outputfile)
-  cat(" Results ready!\n")
+  cat(" ################################# \n # \n # Results Ready!\n # \n ################################# \n")
+
+
+  #if(print.time){
+  #cat(" ################################# \n # \n #   Print time \n # \n ################################# \n")
+    #}
+    running.time <- proc.time()[3] - start.time 
+    if (running.time < 60){
+      out.run.time <- round(running.time, digits = 2)
+      out.time.units <- "seconds"
+    }
+    if (running.time > 60 & running.time < 3600){
+      out.run.time <- round((running.time/60), digits = 2)
+      out.time.units <- "minutes"
+    }
+    if (running.time > 3600){
+      out.run.time <- round((running.time/3600), digits = 2)
+      out.time.units <- "hours"
+    }
+    #if(print.time){
+      print(paste("RUNNING TIME: ",   out.run.time, out.time.units, sep = " "))
+    #}
+    quit()
+  #}
 }
